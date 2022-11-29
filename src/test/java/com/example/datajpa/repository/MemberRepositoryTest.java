553fd21588779ebb22404259877f2643d71b147b
Dowoon <woodw920@gmail.com>
2022-11-29 16:56:31 +0900

    순수 JPA 기반 리포지토리 만들기
    JPA Data 를 이용하지 않고 순수한 JPA 를 이용해서 리포지토리 만들기 + 테스트 코드 작성

--- src/test/java/com/example/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/datajpa/repository/MemberRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.example.datajpa.repository;
 
 import com.example.datajpa.entity.Member;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
