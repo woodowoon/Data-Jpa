eb719db0c5c1a445ccebc3b0d3ffbdac0d1a8212
Dowoon <woodw920@gmail.com>
2022-11-29 14:28:37 +0900

    프로젝트 기본세팅 및 테스트
    프로젝트 기본 세팅을 하고 JPA 테스트를 간단히 진행하였다.

---
+++ src/test/java/com/example/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.example.datajpa.repository;
+
+import com.example.datajpa.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+
+    @Autowired MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member("memberA");
+
+        // when
+        Member saveMember = memberJpaRepository.save(member);
+        Member findMember = memberJpaRepository.find(saveMember.getId());
+
+        // then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+    }
+}
---
+++ src/test/java/com/example/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.example.datajpa.repository;
+
+import com.example.datajpa.entity.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void testMember() {
+        // given
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);
+
+        // when
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
+
+        // then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+    }
+}
