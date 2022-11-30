a52882bed636290296462b8570251e2441341ff3
Dowoon <woodw920@gmail.com>
2022-11-30 13:37:04 +0900

    JPA NamedQuery

--- src/main/java/com/example/datajpa/repository/MemberRepository.java
+++ src/main/java/com/example/datajpa/repository/MemberRepository.java
@@ -2,10 +2,12 @@ package com.example.datajpa.repository;
 
 import com.example.datajpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
     List<Member> findHelloBy();
+    List<Member> findByUsername(@Param("username") String username);
 }
