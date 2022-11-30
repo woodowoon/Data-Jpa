48720fdab3db2c83aba2c74c0471ca92ec7bf67a
Dowoon <woodw920@gmail.com>
2022-11-30 10:03:13 +0900

    메소드 이름으로 쿼리생성

--- src/main/java/com/example/datajpa/repository/MemberRepository.java
+++ src/main/java/com/example/datajpa/repository/MemberRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    List<Member> findByUsername(String username);
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+    List<Member> findHelloBy();
 }
