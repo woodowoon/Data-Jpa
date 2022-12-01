4fa97f9fcdfb9b21b9248eda8b10c0b48b049acd
Dowoon <woodw920@gmail.com>
2022-12-01 14:30:27 +0900

    Web 확장 - 도메인 클래스 컨버터
    간단한 쿼리나 조회용으로는 쓸 수 있어도 왠만해서는 사용하지 않는 것을 추천한다.

---
+++ src/main/java/com/example/datajpa/controller/MemberController.java
@@ -0,0 +1,35 @@
+package com.example.datajpa.controller;
+
+import com.example.datajpa.entity.Member;
+import com.example.datajpa.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("userA"));
+    }
+
+}
